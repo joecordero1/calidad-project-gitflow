9dd8eec2dab23756888e52f853d4c3ba175fa38a
Ariel454 <ariel.raura@udla.edu.ec>
2025-01-12 21:54:10 -0500

    Release comment in Main class implemented

--- Main.java
+++ Main.java
@@ -1,5 +1,9 @@
 public class Main {
     public static void main(String[] args) {
+        System.out.println("***************");
+        System.out.println("* JAVA PROJECT *");
+        System.out.println("***************");
+
         Saludo saludo = new Saludo();
         System.out.println(saludo.generarSaludo("Mundo"));
         System.out.println("Versión 1.0.0 lista para producción.");
